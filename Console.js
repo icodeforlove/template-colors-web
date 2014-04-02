fc2a7c6204e1f37f430887148a79359b98382609
Chad Scira <chadvscira@gmail.com>
2014-04-01 18:16:43 -0700

    started migration from previous structure

--- Console.js
+++ Console.js
@@ -1,240 +1,315 @@
-if (!Array.prototype.forEach)
-{
-	Array.prototype.forEach = function(fun /*, thisArg */)
-	{
-		'use strict';
-
-		if (this === void 0 || this === null) {
-			throw new TypeError();
-		}
-
-		var t = Object(this);
-		var len = t.length >>> 0;
-		if (typeof fun !== "function") {
-			throw new TypeError();
-		}
-
-		var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-		for (var i = 0; i < len; i++) {
-			if (i in t) {
-				fun.call(thisArg, t[i], i, t);
-			}
-		}
-	};
-}
-
 var Console = (function () {
-	var Console = {};
+	var consoleMethodNames = ['log', 'group', 'groupCollapsed', 'groupEnd', 'warn', 'info'];
 
-	var browser = {};
-	browser.isFirefox = /firefox/i.test(navigator.userAgent);
-	browser.isIE = document.documentMode;
+	// preserve original console
+	var consoleReference = console;
+		consoleMethodReferences = {};
 
-	var support = {};
+	consoleMethodNames.forEach(function (name) {
+		consoleMethodReferences[name] = consoleReference[name];
+	});
 
-	function detectSupport () {
-		support.consoleApply = !browser.isIE || document.documentMode && document.documentMode > 9;
-		support.functionGetters = support.consoleApply;
-		support.console = !!window.console;
-		support.modifiedConsole = !browser.isIE && support.console && console.log.toString().indexOf('apply') !== -1;
-		support.consoleColors = !!window.chrome || !!(browser.isFirefox && support.modifiedConsole);
-		support.consoleGroups = !!(window.console && console.group);
+	// general way to calling console methods
+	function consoleApplyMethod (method, args) {
+		args = Array.prototype.slice.call(args);
 
-		if (browser.isFirefox && !support.modifiedConsole) {
-			support.consoleGroups = false;
-			support.consoleApply = false;
+		args = Console.styles.argumentsToConsoleArguments(args);
+
+		// groupEnd should only be proxied if its actually supported
+		if (!consoleMethodReferences[method] && method === 'groupEnd') {
+			return;
 		}
+
+		// if a method is not supported it falls back to a standard log
+		if (!consoleMethodReferences[method]) {
+			method = 'log';
+		}
+
+		consoleMethodReferences[method].apply(consoleReference, args);
 	}
 
-	var Colors = (function () {
-		var existingStyleSpanRegExp = /^<span style="([^"]+)">.+<\/span>$/,
-			styleSpanOpenRegExp = /^<span style="([^"]+)">/,
-			styleSpanOpenOrCloseRegExp = /<span style="[^"]+">|<\/span>/g;
+	// public interface
+	return {
+		log: function () {
+			consoleApplyMethod('log', arguments);
+		},
 
-		function registerStyle (name, style) {
-			function getter () {
-				var string = this.toString();
+		group: function () {
+			consoleApplyMethod('group', arguments);
+		},
 
-				if (!support.consoleColors) {
-					return string;
-				}
+		groupCollapsed: function () {
+			consoleApplyMethod('groupCollapsed', arguments);
+		},
 
-				if (existingStyleSpanRegExp.test(string)) {
-					string = string.replace(existingStyleSpanRegExp, function (match, styles) {
-						if (!styles.match(style)) {
-							return match.replace(styleSpanOpenRegExp, '<span style="' + styles + style + ';">');
-						} else {
-							return match;
-						}
-					});
-				} else {
-					string = '<span style="' + style + ';">' + this + '</span>';
-				}
+		groupEnd: function () {
+			consoleApplyMethod('group', arguments);
+		},
 
-				return string;
-			}
+		warn: function () {
+			consoleApplyMethod('warn', arguments);
+		},
 
-			if (!support.functionGetters && !Console.legacySupport) {
-				String.prototype[name] = '<Console.js:INVALID_GETTER_ATTEMPT>';
-			} else if (Console.legacySupport) {
-				getter.toString = function () {
-					return '<Console.js:INVALID_GETTER_ATTEMPT>';
-				};
+		info: function () {
+			consoleApplyMethod('info', arguments);
+		},
 
-				String.prototype[name] = getter;
-			} else if (Object.defineProperty) {
-				Object.defineProperty(String.prototype, name, {get: getter});
-			} else if (String.prototype.__defineGetter__) {
-				String.prototype.__defineGetter__(name, getter);
-			} else {
-				String.prototype[name] = '';
-			}
+		unknown: function () {
+			consoleApplyMethod('unknown', arguments);
+		},
+
+		attach: function () {
+			consoleMethodNames.forEach(function (method) {
+				window.console['_' + method] = consoleMethodReferences[method];
+				window.console[method] = this[method];
+			}, this);
+		},
+
+		detach: function () {
+			consoleMethodNames.forEach(function (method) {
+				delete window.console['_' + method];
+				window.console[method] = consoleMethodReferences[method];
+			}, this);
 		}
-
-		function registerStyles (styles) {
-			for (var name in styles) {
-				registerStyle(name, styles[name]);
-			}
-		}
-
-		function stringToFormatArray (string) {
-			var colors = [];
-
-			string = string.replace(styleSpanOpenOrCloseRegExp, function (tag) {
-				var styleMatch = tag.match(styleSpanOpenRegExp);
-
-				if (styleMatch) {
-					colors.push(styleMatch[1]);
-				} else {
-					colors.push('');
-				}
-
-				return '%c';
-			});
-
-			return [string].concat(colors);
-		}
-
-		function argumentsToConsoleArguments (args) {
-			var params = [];
-
-			args.forEach(function (arg) {
-				if (typeof arg === 'string') {
-					params = params.concat(stringToFormatArray(arg));
-				} else {
-					params.push(arg);
-				}
-			});
-
-			return params;
-		}
-
-
-		return {
-			registerStyle: registerStyle,
-			registerStyles: registerStyles,
-			argumentsToConsoleArguments: argumentsToConsoleArguments
-		};
-	})();
-
-	function attach () {
-		var methodNames = ['log', 'group', 'groupCollapsed', 'warn', 'info'],
-			groupLevel = 0;
-
-		// browser compatibility
-		if (!support.console) {
-			window.console = {log: function () {}};
-		}
-
-		// groupEnd not supported
-		if (!support.consoleGroups) {
-			if (console.group) {
-				delete console.group;
-				delete console.groupCollapsed;
-				delete console.groupEnd;
-			}
-
-			var groupEnd = console.groupEnd ? console.groupEnd.bind(console) : null;
-			console.groupEnd = function () {
-				groupLevel--;
-				if (groupEnd) {
-					groupEnd();
-				}
-			};
-		}
-
-		methodNames.forEach(function (name) {
-			var method = console['_' + name] = console[name] || console.log;
-
-			console[name] = Console[name] = function () {
-				if (!Console.logging) {
-					return;
-				}
-
-				var args = Array.prototype.slice.call(arguments);
-
-				if (!support.consoleGroups) {
-					if (name === 'group' || name === 'groupCollapsed') {
-						groupLevel++;
-					} else if (name === 'log') {
-						if (groupLevel) {
-							var groupPadding = '';
-							for (var i = 0; i < groupLevel; i++) {
-								groupPadding += '-';
-							}
-							groupPadding += ' ';
-							args.splice(0, 0, groupPadding);
-						}
-					}
-				}
-
-				if (support.consoleApply) {
-					return method.apply(console, Colors.argumentsToConsoleArguments(args));
-				} else {
-					var message = Colors.argumentsToConsoleArguments(args).join(' ');
-
-					if (message.match('<Console.js:INVALID_GETTER_ATTEMPT>')) {
-						if (!Console.legacySupport) {
-							return;
-						}
-
-						message = 'Error Console.js: you need to call your style() methods';
-					}
-
-					return method(message);
-				}
-			};
-		});
 	};
-
-	function detatch () {
-		for (var methodName in console) {
-			if (methodName.substr(0, 1) === '_' && console[methodName.substr(1)]) {
-				if (console.__proto__ && console.__proto__.log) {
-					delete console[methodName.substr(1)];
-				} else {
-					console[methodName.substr(1)] = console[methodName];
-				}
-
-				delete console[methodName];
-			}
-		}
-	}
-
-	detectSupport();
-
-	if (!window.DO_NOT_AUTO_ATTACH_CONSOLE) {
-		attach();
-	}
-
-	Console.detectSupport = detectSupport;
-	Console.attach = attach;
-	Console.detatch = detatch;
-	Console.support = support;
-	Console.logging = true;
-	Console.legacySupport = false;
-	Console.registerStyle = Colors.registerStyle;
-	Console.registerStyles = Colors.registerStyles;
-
-	return Console;
 })();
+
+
+//'log', 'group', 'groupCollapsed', 'warn', 'info'
+
+
+// var Console = (function () {
+// 	var Console = {};
+
+// 	var browser = {};
+// 	browser.isFirefox = /firefox/i.test(navigator.userAgent);
+// 	browser.isIE = document.documentMode;
+
+// 	var support = {};
+
+// 	function detectSupport () {
+// 		support.consoleApply = !browser.isIE || document.documentMode && document.documentMode > 9;
+// 		support.functionGetters = support.consoleApply;
+// 		support.console = !!window.console;
+// 		support.modifiedConsole = !browser.isIE && support.console && console.log.toString().indexOf('apply') !== -1;
+// 		support.consoleColors = !!window.chrome || !!(browser.isFirefox && support.modifiedConsole);
+// 		support.consoleGroups = !!(window.console && console.group);
+
+// 		if (browser.isFirefox && !support.modifiedConsole) {
+// 			support.consoleGroups = false;
+// 			support.consoleApply = false;
+// 		}
+// 	}
+
+// 	function Stack (stack) {
+// 		this._stackString = stack || new Error().stack || '';
+
+// 	}
+// 	Stack.prototype = {
+// 		_geckoStackWithMethodNameRegExp: /\b([a-z0-9_-]+)@.*\/([^\/]+)\:(\d*)$/i,
+// 		_geckoStackWithoutMethodName: /@.*\/([^\/]+)\:(\d*)$/i,
+
+// 		_webkitStackWithMethodNameRegExp: /.+\b([a-z0-9_-]+) \(.*\/([^\/]+)\:(\d*)\:(\d+)\)$/i,
+// 		_webkitStackWithoutMethodName: /at .*\/([^\/]+)\:(\d*)\:(\d+)/i,
+
+// 		parse: function () {
+// 			var stack = this._stackString;
+
+// 			// convert stack into an array
+// 			stack = stack.split('\n');
+
+// 			// pop off first item
+// 			stack = stack.slice(1);
+
+// 			// parse stack
+// 			stack = stack.map(function (line) {
+// 				return this._parseStackLine(line);
+// 			}, this);
+
+// 			return stack || null;
+// 		},
+
+// 		_parseStackLine: function (line) {
+// 			var parsedLine,
+// 				userAgent = navigator.userAgent;
+
+// 			if (userAgent.match(/Webkit/i)) {
+// 			 	parsedLine = this._webkitParseStackLine(line);
+// 			} else if (userAgent.match(/Gecko/i)) {
+// 				parsedLine = this._geckoParseStackLine(line);
+// 			}
+// 			return parsedLine || {string: line};
+// 		},
+
+// 		_geckoParseStackLine: function (line) {
+// 			var match;
+
+// 			if (this._geckoStackWithMethodNameRegExp.test(line)) {
+// 				match = line.match(this._geckoStackWithMethodNameRegExp);
+
+// 				return {
+// 					methodName: match[1],
+// 					fileName: match[2],
+// 					lineNumber: match[3]
+// 				};
+// 			} else if (this._geckoStackWithoutMethodName.test(line)) {
+// 				match = line.match(this._geckoStackWithoutMethodName);
+
+// 				return {
+// 					fileName: match[1],
+// 					lineNumber: match[2]
+// 				};
+// 			}
+// 		},
+
+// 		_webkitParseStackLine: function (line) {
+// 			var match;
+
+// 			if (this._webkitStackWithMethodNameRegExp.test(line)) {
+// 				match = line.match(this._webkitStackWithMethodNameRegExp);
+
+// 				return {
+// 					methodName: match[1],
+// 					fileName: match[2],
+// 					lineNumber: match[3],
+// 					columnNumber: match[4]
+// 				};
+// 			} else if (this._webkitStackWithoutMethodName.test(line)) {
+// 				match = line.match(this._webkitStackWithoutMethodName);
+
+// 				return {
+// 					fileName: match[1],
+// 					lineNumber: match[2],
+// 					columnNumber: match[3]
+// 				};
+// 			}
+// 		},
+
+// 		getLineByCaller: function (caller, offset) {
+// 			offset = offset || 0;
+
+// 			var stack = this.parse();
+
+// 			if (!stack) {
+// 				return '';
+// 			}
+
+// 			for (var i = 0; i < stack.length; i++) {
+// 				if (stack[i] && caller === stack[i].methodName) {
+// 					return stack[i+offset];
+// 				}
+// 			}
+
+// 			return null;
+// 		}
+// 	};
+
+
+
+// 	function attach () {
+// 		var methodNames = ['log', 'group', 'groupCollapsed', 'warn', 'info'],
+// 			groupLevel = 0;
+
+// 		// browser compatibility
+// 		if (!support.console) {
+// 			window.console = {log: function () {}};
+// 		}
+
+// 		// groupEnd not supported
+// 		if (!support.consoleGroups) {
+// 			if (console.group) {
+// 				delete console.group;
+// 				delete console.groupCollapsed;
+// 				delete console.groupEnd;
+// 			}
+
+// 			var groupEnd = console.groupEnd ? console.groupEnd.bind(console) : null;
+// 			console.groupEnd = function () {
+// 				groupLevel--;
+// 				if (groupEnd) {
+// 					groupEnd();
+// 				}
+// 			};
+// 		}
+
+// 		methodNames.forEach(function (name) {
+// 			var method = console['_' + name] = console[name] || console.log;
+
+// 			console[name] = Console[name] = function () {
+// 				if (!Console.logging) {
+// 					return;
+// 				}
+
+// 				var args = Array.prototype.slice.call(arguments);
+
+// 				if (!support.consoleGroups) {
+// 					if (name === 'group' || name === 'groupCollapsed') {
+// 						groupLevel++;
+// 					} else if (name === 'log') {
+// 						if (groupLevel) {
+// 							var groupPadding = '';
+// 							for (var i = 0; i < groupLevel; i++) {
+// 								groupPadding += '-';
+// 							}
+// 							//groupPadding += ' ';
+// 							args.splice(0, 0, groupPadding);
+// 						}
+// 					}
+// 				}
+
+// 				if (support.consoleApply) {
+// 					return method.apply(console, Console.styles.argumentsToConsoleArguments(args));
+// 				} else {
+// 					var message = Console.styles.argumentsToConsoleArguments(args).join(' ');
+
+// 					if (message.match('<Console.js:INVALID_GETTER_ATTEMPT>')) {
+// 						if (!Console.legacySupport) {
+// 							return;
+// 						}
+
+// 						message = 'Error Console.js: you need to call your style() methods';
+// 					}
+
+// 					return method(message);
+// 				}
+// 			};
+// 		});
+// 	};
+
+// 	function detatch () {
+// 		for (var methodName in console) {
+// 			if (methodName.substr(0, 1) === '_' && console[methodName.substr(1)]) {
+// 				if (console.__proto__ && console.__proto__.log) {
+// 					delete console[methodName.substr(1)];
+// 				} else {
+// 					console[methodName.substr(1)] = console[methodName];
+// 				}
+
+// 				delete console[methodName];
+// 			}
+// 		}
+// 	}
+
+// 	detectSupport();
+
+// 	Console.detectSupport = detectSupport;
+// 	Console.attach = attach;
+// 	Console.detatch = detatch;
+// 	Console.support = support;
+// 	Console.logging = true;
+// 	Console.legacySupport = false;
+// 	// Console.registerStyle = Colors.registerStyle;
+// 	// Console.registerStyles = Colors.registerStyles;
+// 	Console.getFileAndLineNumber = function (caller, offset) {
+// 		var stack = new Stack(),
+// 			line = stack.getLineByCaller(caller, offset);
+
+// 		if (line) {
+// 			return line.fileName + ':' + line.lineNumber + ' '
+// 		} else {
+// 			return '';
+// 		}
+// 	};
+
+// 	return Console;
+// })();
---
+++ Styles.js
@@ -0,0 +1,101 @@
+Console.styles = (function () {
+	var existingSpanRegExp = /^<span style="([^"]+)">.+<\/span>$/,
+		spanOpenRegExp = /^<span style="([^"]+)">/,
+		spanOpenOrCloseRegExp = /<span style="[^"]+">|<\/span>/g,
+		styles = {},
+		attached = false;
+
+	function attach () {
+		attached = true;
+	}
+
+	function register () {
+		if (typeof arguments[0] === 'object') {
+			var styles = arguments[0];
+
+			for (var name in styles) {
+				if (!styles.hasOwnProperty(name)) return;
+				registerStyle(name, styles[name]);
+			}
+		} else {
+			registerStyle(arguments[0], arguments[1]);
+		}
+	}
+
+	function registerStyle (name, style) {
+		styles[name] = style;
+
+		if (attach) {
+			function getter () {
+				return format(this.toString(), name);
+			}
+
+			if (Object.defineProperty) {
+				Object.defineProperty(String.prototype, name, {get: getter});
+			} else if (String.prototype.__defineGetter__) {
+				String.prototype.__defineGetter__(name, getter);
+			} else {
+				String.prototype[name] = '';
+			}
+		}
+	}
+
+	function format (string, names) {
+		names.split(',').forEach(function (name) {
+			var style = styles[name];
+
+			if (existingSpanRegExp.test(string)) {
+				string = string.replace(existingSpanRegExp, function (match, styles) {
+					if (!styles.match(style)) {
+						return match.replace(spanOpenRegExp, '<span style="' + styles + style + ';">');
+					} else {
+						return match;
+					}
+				});
+			} else {
+				string = '<span style="' + style + ';">' + string + '</span>';
+			}
+		});
+
+		return string;
+	}
+
+	function stringToFormatArray (string) {
+		var colors = [];
+
+		string = string.replace(spanOpenOrCloseRegExp, function (tag) {
+			var styleMatch = tag.match(spanOpenRegExp);
+
+			if (styleMatch) {
+				colors.push(styleMatch[1]);
+			} else {
+				colors.push('');
+			}
+
+			return '%c';
+		});
+
+		return [string].concat(colors);
+	}
+
+	function argumentsToConsoleArguments (args) {
+		var params = [];
+
+		args.forEach(function (arg) {
+			if (typeof arg === 'string') {
+				params = params.concat(stringToFormatArray(arg));
+			} else {
+				params.push(arg);
+			}
+		});
+
+		return params;
+	}
+
+	return {
+		attach: attach,
+		format: format,
+		register: register,
+		argumentsToConsoleArguments: argumentsToConsoleArguments
+	};
+})();
